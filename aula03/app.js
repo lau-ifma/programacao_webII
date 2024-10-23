f68c583597bd5a6590287007ce66460d38250da5
Lauanderson Rael Costa Gomes <20232ADS.CNT0002@ifma.edu>
2024-10-23 20:13:04 -0300

    added calculator

---
+++ aula03/app.js
@@ -0,0 +1,9 @@
+
+const Somar = (a, b) =>{
+    s = a + b
+    return s
+}
+
+const res = Somar(3,4)
+
+console.log(res)
---
+++ aula03/calculadora.js
@@ -0,0 +1,43 @@
+const adicao = (a, b) =>{
+    let s = parseFloat(a) + parseFloat(b)
+    return s
+}
+const Subtrair = (a, b) =>{
+    s = a - b
+    return s
+}
+const Multiplicar = (a, b) =>{
+    s = a * b
+    return s
+}
+const Dividir = (a, b) =>{
+    s = a / b
+    return s
+}
+
+function clicouSoma(){
+    let v1 = document.getElementById("v1").value;
+    let v2 = document.getElementById("v2").value;
+
+    let s = adicao(v1, v2)
+
+    document.getElementById("resultado").value = s
+}
+function clicouSub(){
+    let v1 = document.getElementById("v1").value;
+    let v2 = document.getElementById("v2").value;
+    let s = Subtrair(v1, v2)
+    document.getElementById("resultado").value = s
+}
+function clicouMult(){
+    let v1 = document.getElementById("v1").value;
+    let v2 = document.getElementById("v2").value;
+    let s = Multiplicar(v1, v2)
+    document.getElementById("resultado").value = s
+}
+function clicouDiv(){
+    let v1 = document.getElementById("v1").value;
+    let v2 = document.getElementById("v2").value;
+    let s = Dividir(v1, v2)
+    document.getElementById("resultado").value = s
+}
