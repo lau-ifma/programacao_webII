0fd19a5e9ae25816d67137774b4b0994ef210f19
lau-ifma <lauandersoncosta@acad.ifma.edu.br>
2024-11-27 19:58:35 -0300

    aula de rotas

--- aula08-express/projeto01/index.js
+++ aula08-express/projeto01/index.js
@@ -1,13 +1,26 @@
 import express from "express";
 const app = express();
 
+const porta = 8000
+
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.send('Hello world my friend')
+})
+
+app.get('/cliente', (req, res) => {
+    res.send('Rota de cliente')
+})
+app.get('/produto/:id', (req, res) => {
+    let codigo = req.params.id
+    res.send('Meu produto:' + codigo * 2)
+})
+
+app.get('*', (req, res) => {
+    res.send('Pagina não encontrada!')
 })
 
 
-
-// fica no final
-app.listen(3000, ()=>{
-    console.log("Servidor rodando na porta 3000")
+// fica no final 
+app.listen(porta, ()=>{
+    console.log(`Servidor rodando em http://localhost:${porta}`)
 });
