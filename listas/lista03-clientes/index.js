a4c3ecf0ba9588c6d1ef674a45393e77af55c9ba
lauanderson-rael <lauanderson38@gmail.com>
2024-12-04 16:51:29 -0300

    ccc

---
+++ listas/lista03-clientes/index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const app = express()
+const port = 3000
+
+// configurar onde estao ficarão os arquivo independente de servidor
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
+
+
+
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/contents/index.html')
+})
+
+app.get('/listar', (req, res)=>{
+    res.sendFile(__dirname+'/contents/listar.html')
+})
+
+app.get('/cadastrar', (req, res)=>{
+    res.sendFile(__dirname+'/contents/cadastrar.html')
+})
+app.get('/usuarios', (req, res)=>{
+    res.sendFile(__dirname+'/contents/usuarios.html')
+})
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Servidor rodando em http://localhost:${port}`)
+})
