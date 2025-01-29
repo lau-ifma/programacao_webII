b8c037c5d380104a6bea9fab24032628ddcdae56
lau-ifma <lauandersoncosta@acad.ifma.edu.br>
2025-01-29 20:10:48 -0300

    created users

--- projetos/estoque_ads/controllers/UsuarioController.js
+++ projetos/estoque_ads/controllers/UsuarioController.js
@@ -1,9 +1,30 @@
 import Usuario from "../models/Usuario.js";
+import Pessoa from "../models/Pessoa.js";
 
 class UsuarioController {
     index = async (req, res) => {
         let usuarios = await Usuario.findAll()
         res.render('usuario/index', { usuarios: usuarios })
+    }
+
+    cadastrar = async (req, res) =>{
+        const pessoas = await Pessoa.findAll()
+        res.render('usuario/cadastro', {pessoas: pessoas})
+    }
+
+    salvar = (req, res) => {
+        const { username, senha, categoria, pessoa_id  } = req.body;
+        const novo_usuario = {
+            username: username,
+            senha: senha,
+            categoria: categoria,
+            pessoa_id: pessoa_id,
+            status: 1
+        }
+        Usuario.create(novo_usuario).then(() => {
+            console.log("Usuario cadastrado com Sucesso!")
+            res.redirect('/usuario')
+        })
     }
 }
 
--- projetos/estoque_ads/routes/usuario.js
+++ projetos/estoque_ads/routes/usuario.js
@@ -4,5 +4,7 @@ const router = express.Router()
 import UsuarioController from '../controllers/UsuarioController.js'
 
 router.get('/', UsuarioController.index)
+router.get('/cadastro', UsuarioController.cadastrar)
+router.post('/salvar', UsuarioController.salvar)
 
 export default router
